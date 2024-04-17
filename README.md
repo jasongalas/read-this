7e1dbe622968f2d46201e3eb80c47749148cc1bd
jasongalas <jasongalas@gmail.com>
2024-04-16 20:54:48 -0700

    created the javascript code to write the readme with a test readme. will be replacing this with the legit one later, also still working on the licensing code

---
+++ README.md
@@ -0,0 +1,39 @@
+# oy
+
+  ##Description
+
+  vey
+
+  ##Table of Contents
+
+  -[Installation](#installation)
+  -[Usage](#Usage)
+  -[Credits](#Credits)
+  -[License](#License)
+  -[Tests](#Tests)
+  -[Questions](#Questions)
+
+  ##Installation
+
+  so
+
+  ##Usage
+
+  much
+
+  ##Credits
+
+  typing
+
+  ##License
+
+  MIT
+
+  ##Tests
+
+  involved
+
+  ##Questions
+
+  My [Github] is (https://github.com/jasongalas)
+  My email is (jasongalas@gmail.com)
--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      message: 'What is your Project Title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Please describe your project.',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Please describe the installation process of your project.',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Please describe the usage of your project.',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Please list your contributors.',
+      name: 'credits',
+    },
+    {
+      type: 'list',
+      message: 'Please choose the license you used.',
+      name: 'license',
+      choices: ['APACHE', 'Boost', 'Eclipse', 'MIT', 'none'],
+    },
+    {
+      type: 'input',
+      message: 'Please list any tests, if applicable.',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'Please give your github username.',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'Please give your email.',
+      name: 'email',
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+fs.writeFile(fileName, data, (err) => {
+    err ? console.error(err) : console.log('Read Me written!');
+})
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    writeToFile("README.md", generateMarkdown({...inquirerResponses}))
+  })
+}
 
 // Function call to initialize app
 init();
